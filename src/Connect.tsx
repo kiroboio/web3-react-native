67e1ee30a541f98c51fbb07d02f8cc3fe6896367
Ser Fr <bigcheeseh@gmail.com>
2022-02-07 16:50:51 +0200

    collect/retrieve check

--- src/Connect.tsx
+++ src/Connect.tsx
@@ -1,5 +1,4 @@
 import {
-  Connectors,
   useAccount,
   observer,
   configureReactotronDebugging,
@@ -7,15 +6,15 @@ import {
 } from './dev';
 import React, { useEffect } from 'react';
 
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList, ListViewBase } from 'react-native';
 
 export const Connect = observer(() => {
-  const { address, active, connect, balance, block, deposit } = useAccount();
+  const { address, active, connect, balance, block, deposit, retrieve, collect, outgoing, incoming } = useAccount();
   console.log({ address, active, balance, block });
   useEffect(() => {
     configureReactotronDebugging({});
 
-    return connect.run({ chainId: 4, key: '0x9be9b846aba3093b8d5898d796bc8ac74af918120a23304ecf21a5fa22003082'});
+    return connect.run({ chainId: 4, key: '0x9be9b846aba3093b8d5898d796bc8ac74af918120a23304ecf21a5fa22003082' });
   }, []);
 
   const handleDeposit = () => {
@@ -26,11 +25,35 @@ export const Connect = observer(() => {
     });
   };
 
+  const handleRetrieve = (id: string) => {
+    retrieve.run({
+      id,
+    });
+  };
+
+  const handleCollect = (id: string) => {
+    collect.run({
+      id,
+      passcode: '123321'
+    });
+  };
+
+
   if (deposit.is.running) return <Text>Running...</Text>;
+  if (retrieve.is.running) return <Text>Retrieving...</Text>;
+  if (collect.is.running) return <Text>Collecting...</Text>;
   return (
     <View>
       <Text>test</Text>
       <Button title="deposit" onPress={handleDeposit} />
+
+      {incoming.list.map((trx) => {
+        return <Button title="collect" key={trx.id} onPress={() => handleCollect(trx.id)} />
+      })}
+      {outgoing.list.map((trx) => {
+        return <Button title="retrieve" key={trx.id} onPress={() => handleRetrieve(trx.id)} />
+      })}
+
     </View>
   );
 });
