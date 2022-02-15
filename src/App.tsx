5fd9b16060d4a5700afff1c349ac2e6b36a2beb4
Ori Shalom <oris@kirobo.io>
2022-02-15 15:45:03 +0200

    ios fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {KiroboProvider} from '@kiroboio/web3-react-native-safe-transfer';
-import {Connect} from './Connect';
-import {INFURA_KEY, API_KEY, API_SECRET} from 'react-native-dotenv';
+import { KiroboProvider } from '@kiroboio/web3-react-native-safe-transfer';
+import { Connect } from './Connect';
+import { INFURA_KEY, API_KEY, API_SECRET } from 'react-native-dotenv';
 
 export const App = () => {
   return (
